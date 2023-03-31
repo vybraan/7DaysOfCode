55c342281bce7c0009663e345d38da870c1ecd3a
Ailton Baúque <nevskya@hotmail.com>
2023-04-01 00:36:16 +0200

    Day one resources

---
+++ Day_1/Program.cs
@@ -0,0 +1,21 @@
+﻿using RestSharp;
+
+
+class Program{
+
+  public static void Main(){
+  using RestSharp;
+using RestSharp.Authenticators;
+
+var client = new RestClient("https://api.twitter.com/1.1") {
+    Authenticator = new HttpBasicAuthenticator("username", "password")
+};
+var request = new RestRequest("statuses/home_timeline.json");
+var response = await client.GetAsync(request, cancellationToken);
+  }
+
+
+//var request = new RestRequest("statuses/home_timeline.json");
+//var response = await client.GetAsync(request, cancellationToken);
+//
+}
