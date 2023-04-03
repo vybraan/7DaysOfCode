4a4b208f5f1f13a07702755460e3b14e65fee111
Ailton Baúque <nevskya@hotmail.com>
2023-04-03 10:59:37 +0200

    UPDATE: adding package RestSharp

--- Day_2/Program.cs
+++ Day_2/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RestSharp;
+
+
+class Program{
+
+  static void Main(string[] args){
+
+    // string url = "https://pokeapi.co/api/v2/pokemon/";
+    string url = "https://pokeapi.co/api/v2/pokemon/1/";
+
+    var client = new RestClient(url); 
+
+    var request = new RestRequest();
+
+    var response = client.Get(request);
+
+    Console.WriteLine(response.Content.ToString());
+  }
+
+
+}
